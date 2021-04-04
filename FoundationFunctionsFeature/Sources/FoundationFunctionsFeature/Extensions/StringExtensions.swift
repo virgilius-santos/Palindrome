04c4d1f9c9232d14e1d8ea978be686af57e56bf7
Virgilius <virgilius_santos@yahoo.com.br>
2021-04-04 20:40:05 -0300

    add diacriticInsensitive function

--- FoundationFunctionsFeature/Sources/FoundationFunctionsFeature/Extensions/StringExtensions.swift
+++ FoundationFunctionsFeature/Sources/FoundationFunctionsFeature/Extensions/StringExtensions.swift
@@ -10,3 +10,7 @@ public extension String {
 public func onlyAlphanumerics(_ string: String) -> String {
   string.components(separatedBy: CharacterSet.alphanumerics.inverted).joined(separator: "")
 }
+
+public func diacriticInsensitive(_ string: String) -> String {
+  string.folding(options: .diacriticInsensitive, locale: nil)
+}
