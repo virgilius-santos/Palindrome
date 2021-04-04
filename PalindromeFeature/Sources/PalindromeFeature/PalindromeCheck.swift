9fba9539a8e5032b8cb6f35cbb3b5f2bfd41e030
Virgilius <virgilius_santos@yahoo.com.br>
2021-04-04 17:59:12 -0300

    update palindrome check

--- PalindromeFeature/Sources/PalindromeFeature/PalindromeCheck.swift
+++ PalindromeFeature/Sources/PalindromeFeature/PalindromeCheck.swift
@@ -1,12 +1,6 @@
 
 import Foundation
 
-extension String {
-  subscript (i: Int) -> Character {
-    self[index(startIndex, offsetBy: i)]
-  }
-}
-
 public func checkIfIsPalindrome(word: String) -> Bool {
   let word = word.uppercased()
   if word.isEmpty || word.count == 1 {
@@ -17,11 +11,6 @@ public func checkIfIsPalindrome(word: String) -> Bool {
   let mid = word.count/2
   let rx = stride(from: 0, through: mid, by: 1)
   let ry = stride(from: last, through: mid, by: -1)
-  for (j,k) in zip(rx, ry) {
-    if word[j] != word[k] {
-      return false
-    }
-  }
   
-  return true
+  return zip(rx, ry).contains(where: { (j,k) in word[j] != word[k] }) == false
 }
