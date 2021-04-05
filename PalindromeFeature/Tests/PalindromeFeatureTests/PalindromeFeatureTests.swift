ec39e812b8c9cda14e4ab4d2b91bc784ac540048
Virgilius <virgilius_santos@yahoo.com.br>
2021-04-04 21:09:40 -0300

    reallocate mock

--- PalindromeFeature/Tests/PalindromeFeatureTests/PalindromeFeatureTests.swift
+++ PalindromeFeature/Tests/PalindromeFeatureTests/PalindromeFeatureTests.swift
@@ -14,14 +14,3 @@ final class PalindromeFeatureTests: XCTestCase {
     }
   }
 }
-
-extension PalindromeViewModel {
-  static let mock = PalindromeViewModel(
-    isPalindrome: Box<Bool>(true),
-    saveWord: { _ in },
-    deleteWord: { _, _ in },
-    numberOfWords: { 17 },
-    wordFor: { _ in "dummy" },
-    newWord: { _ in }
-  )
-}
